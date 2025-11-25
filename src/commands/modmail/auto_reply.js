2e74173c1b816ba9ed08835046db9fc28bc3f28e
itfahim0 <fahimmuntasir77@gmail.com>
2025-11-26 01:43:46 +0600

    Restore auto_reply.js with safe content

---
+++ src/commands/modmail/auto_reply.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('auto-reply')
+        .setDescription('Send a canned response')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addStringOption(opt =>
+            opt.setName('key')
+                .setDescription('Response key')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Help', value: 'help' },
+                    { name: 'Info', value: 'info' }
+                )),
+
+    async execute(interaction) {
+        await interaction.reply({ content: "This command is temporarily disabled.", ephemeral: true });
+    },
+};
